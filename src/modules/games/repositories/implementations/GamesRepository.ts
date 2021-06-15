1c4502bce3438237edd5fd878cf3fbd751b2eb75
George Armando <georgearmando54@gmail.com>
2021-06-15 22:30:31 +0100

    feat: Create the logic for the methods

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -7,24 +7,42 @@ import { IGamesRepository } from '../IGamesRepository';
 
 export class GamesRepository implements IGamesRepository {
   private repository: Repository<Game>;
+  private usersRepository: Repository<User>;
 
   constructor() {
     this.repository = getRepository(Game);
+    this.usersRepository = getRepository(User);
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder('game')
+      .where('game.title ILIKE :param', { param: `%${param}%` })
+      .getMany()
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+    SELECT COUNT(title) FROM games
+  `);
   }
+
+
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder("games")
+      .relation(Game, "users")
+      .of(id)
+      .loadMany();
   }
+
+  /**
+   *   async findUsersByGameId(id: string): Promise<User[]> {
+    return this.usersRepository
+      .createQueryBuilder('user')
+      .innerJoinAndSelect('user.games', 'game', 'game.id = :id', { id })
+      .getMany()
+  }
+   */
 }
